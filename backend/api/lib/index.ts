7d94c35ae4642edfabfd06e934f1d1ebe749af25
Dadzial <duszakdamian034@gmail.com>
2026-05-07 23:10:53 +0200

    init of controllers

--- backend/api/lib/index.ts
+++ backend/api/lib/index.ts
@@ -1,5 +1,16 @@
 import App from "./app";
+import UserController from "./controllers/user.controller";
+import LightsController from "./controllers/lights.controller";
 
 const app = new App();
+
+const controllers = [
+    new LightsController(),
+    new UserController()
+]
+
+controllers.forEach(controller => {
+    app.app.use("/", controller.router);
+});
 
 app.listen();
